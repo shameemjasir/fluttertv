423860c624accb8df203d388e69d4dc8a8b66458
Shameem Jasir M <shameem.jm@1digitals.com>
2023-06-25 01:28:31 +0300

    feat: focus node

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:fluttertv/screens/home.dart';
 
 void main() {
-  runApp(const Home());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter TV',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const StartPage(title: 'Flutter TV'),
+    );
+  }
+}
+
+class StartPage extends StatelessWidget {
+  const StartPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body:  Home(),
+    );
+  }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,15 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertv/screens/video/playing_screen.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
-
+  Home({super.key});
+  final FocusNode _playButtonFocusNode = FocusNode();
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.black,
-
-      
-
+      color: const Color.fromARGB(255, 252, 252, 252),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 200, top: 100),
+        child: Row(
+          children: [
+            SizedBox(
+              child: ElevatedButton.icon(
+                focusNode: _playButtonFocusNode,
+                onPressed: () {
+                  // Respond to button press
+                },
+                icon: const Icon(Icons.add, size: 18),
+                label: const Text("PlayingScreen"),
+              ),
+            ),
+            const SizedBox(
+              width: 30,
+            ),
+            SizedBox(
+              child: ElevatedButton.icon(
+                
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const PlayingScreen(),
+                    ),
+                  );
+                },
+                icon: const Icon(Icons.add, size: 18),
+                label: const Text("CarouselScreen"),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
