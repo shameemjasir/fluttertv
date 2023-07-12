5a8e5361f1610db65604f7cd30108cd54e445edf
Shameem Jasir M <shameem.jm@1digitals.com>
2023-07-12 16:19:53 +0300

    fix: custom focus

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:fluttertv/screens/video/playing_screen.dart';
-
- class LeftButtonIntent extends Intent{}
- class RightButtonIntent extends Intent{}
-class UpButtonIntent extends Intent{}
-class DownButtonIntent extends Intent{}
-class EnterButtonIntent extends Intent {}
+import 'package:fluttertv/utils/action_handler.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
-
-
 
   @override
   State<Home> createState() => _HomeState();
@@ -20,7 +12,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   final FocusNode _playButtonFocusNode = FocusNode();
   final FocusNode _carouselFocusNode = FocusNode();
-  final IconData _selectdIcon = Icons.laptop;
+  IconData _selectdIcon = Icons.laptop;
   FocusNode? _icon1FocusNode;
   FocusNode? _icon2FocusNode;
   FocusNode? _icon3FocusNode;
@@ -42,8 +34,9 @@ class _HomeState extends State<Home> {
     FocusScope.of(context).requestFocus(_icon1FocusNode);
   }
 
-  void _changeFocus(BuildContext context, FocusNode node) {
+  _changeFocus(BuildContext context, FocusNode node) {
     FocusScope.of(context).requestFocus(node);
+    setState(() {});
   }
 
   @override
@@ -54,6 +47,7 @@ class _HomeState extends State<Home> {
     _icon2FocusNode = FocusNode();
     _icon3FocusNode = FocusNode();
     _icon4FocusNode = FocusNode();
+    super.dispose();
   }
 
   @override
@@ -66,44 +60,97 @@ class _HomeState extends State<Home> {
       color: const Color.fromARGB(255, 252, 252, 252),
       child: Padding(
         padding: const EdgeInsets.only(left: 200, top: 100),
-        child: Shortcuts(
-          shortcuts: <LogicalKeySet, Intent>{
-            LogicalKeySet(LogicalKeyboardKey.arrowLeft):  LeftButtonIntent(),
-            LogicalKeySet(LogicalKeyboardKey.arrowRight): RightButtonIntent(),
-            LogicalKeySet(LogicalKeyboardKey.arrowUp):    UpButtonIntent(),
-            LogicalKeySet(LogicalKeyboardKey.arrowDown):  DownButtonIntent(),
-            LogicalKeySet(LogicalKeyboardKey.select):     EnterButtonIntent(),
-          },
+        child: ActionHandler().handleArrowsandEnterActions(
           child: Column(
             children: [
               Row(
                 children: [
                   SizedBox(
-                    child: ElevatedButton.icon(
-                      focusNode: _playButtonFocusNode,
-                      onPressed: () {
-                        print('object');
+                    child: Actions(
+                      actions: <Type, Action<Intent>>{
+                        LeftButtonIntent: CallbackAction<LeftButtonIntent>(
+                            onInvoke: (Intent) {
+                          _selectdIcon = Icons.tablet_android;
+                          return _changeFocus(context, _icon4FocusNode!);
+                        }),
+                        RightButtonIntent: CallbackAction<RightButtonIntent>(
+                            onInvoke: (Intent) {
+                          _selectdIcon = Icons.image;
+                          return _changeFocus(context, _carouselFocusNode);
+                        }),
+                        UpButtonIntent:
+                            CallbackAction<UpButtonIntent>(onInvoke: (Intent) {
+                          _selectdIcon = Icons.desktop_windows;
+                          return _changeFocus(context, _icon2FocusNode!);
+                        }),
+                        DownButtonIntent: CallbackAction<DownButtonIntent>(
+                            onInvoke: (Intent) {
+                          _selectdIcon = Icons.laptop;
+                          return _changeFocus(context, _icon1FocusNode!);
+                        }),
                       },
-                      icon: const Icon(Icons.add, size: 18),
-                      label: const Text("PlayingScreen"),
+                      child: ElevatedButton.icon(
+                        style: ButtonStyle(
+                          backgroundColor: !_playButtonFocusNode.hasFocus
+                              ? null
+                              : MaterialStateProperty.all<Color>(
+                                  const Color.fromARGB(255, 13, 82, 139)),
+                        ),
+                        focusNode: _playButtonFocusNode,
+                        onPressed: () {
+                          print('object');
+                        },
+                        icon: const Icon(Icons.add, size: 18),
+                        label: const Text("PlayingScreen"),
+                      ),
                     ),
                   ),
                   const SizedBox(
                     width: 30,
                   ),
                   SizedBox(
-                    child: ElevatedButton.icon(
-                      focusNode: _carouselFocusNode,
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const PlayingScreen(),
-                          ),
-                        );
+                    child: Actions(
+                      actions: <Type, Action<Intent>>{
+                        LeftButtonIntent: CallbackAction<LeftButtonIntent>(
+                            onInvoke: (Intent) {
+                          _selectdIcon = Icons.image;
+                          return _changeFocus(context, _playButtonFocusNode);
+                        }),
+                        RightButtonIntent: CallbackAction<RightButtonIntent>(
+                            onInvoke: (Intent) {
+                          _selectdIcon = Icons.tablet_android;
+                          return _changeFocus(context, _icon4FocusNode!);
+                        }),
+                        UpButtonIntent:
+                            CallbackAction<UpButtonIntent>(onInvoke: (Intent) {
+                          _selectdIcon = Icons.desktop_windows;
+                          return _changeFocus(context, _icon2FocusNode!);
+                        }),
+                        DownButtonIntent: CallbackAction<DownButtonIntent>(
+                            onInvoke: (Intent) {
+                          _selectdIcon = Icons.laptop;
+                          return _changeFocus(context, _icon1FocusNode!);
+                        }),
                       },
-                      icon: const Icon(Icons.add, size: 18),
-                      label: const Text("CarouselScreen"),
+                      child: ElevatedButton.icon(
+                        focusNode: _carouselFocusNode,
+                        style: ButtonStyle(
+                          backgroundColor: !_carouselFocusNode.hasFocus
+                              ? null
+                              : MaterialStateProperty.all<Color>(
+                                  const Color.fromARGB(255, 6, 117, 84)),
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const PlayingScreen(),
+                            ),
+                          );
+                        },
+                        icon: const Icon(Icons.add, size: 18),
+                        label: const Text("CarouselScreen"),
+                      ),
                     ),
                   ),
                 ],
@@ -118,75 +165,178 @@ class _HomeState extends State<Home> {
               const SizedBox(height: 10),
               Row(
                 children: [
-                  Container(
-                    padding: const EdgeInsets.all(12),
-                    decoration: !_icon1FocusNode!.hasFocus
-                        ? null
-                        : BoxDecoration(
-                            border: Border.all(
-                              width: 5,
-                              color: const Color.fromARGB(255, 41, 141, 255),
-                            ),
-                          ),
-                    child: const Icon(
-                      Icons.laptop,
-                      size: 40,
-                    ),
-                  ),
-                  const SizedBox(width: 20),
-                  Focus(
-                    focusNode: _icon2FocusNode,
-                    child: Container(
-                      padding: const EdgeInsets.all(12),
-                      decoration: BoxDecoration(
-                        border: !_icon2FocusNode!.hasFocus
+                  Actions(
+                    actions: <Type, Action<Intent>>{
+                      LeftButtonIntent:
+                          CallbackAction<LeftButtonIntent>(onInvoke: (Intent) {
+                        _selectdIcon = Icons.tablet_android;
+                        return _changeFocus(context, _icon4FocusNode!);
+                      }),
+                      RightButtonIntent:
+                          CallbackAction<RightButtonIntent>(onInvoke: (Intent) {
+                        _selectdIcon = Icons.desktop_windows;
+                        return _changeFocus(context, _icon2FocusNode!);
+                      }),
+                      UpButtonIntent:
+                          CallbackAction<UpButtonIntent>(onInvoke: (Intent) {
+                        _selectdIcon = Icons.play_arrow;
+                        return _changeFocus(context, _playButtonFocusNode);
+                      }),
+                       DownButtonIntent:
+                          CallbackAction<DownButtonIntent>(onInvoke: (Intent) {
+                        _selectdIcon = Icons.image;
+                        return _changeFocus(context, _carouselFocusNode);
+                      }),
+                    },
+                    child: Focus(
+                      focusNode: _icon1FocusNode,
+                      child: Container(
+                        padding: const EdgeInsets.all(12),
+                        decoration: !_icon1FocusNode!.hasFocus
                             ? null
-                            : Border.all(
-                                width: 5,
-                                color: const Color.fromARGB(255, 41, 141, 255),
+                            : BoxDecoration(
+                                border: Border.all(
+                                  width: 5,
+                                  color:
+                                      const Color.fromARGB(255, 41, 141, 255),
+                                ),
                               ),
-                      ),
-                      child: const Icon(
-                        Icons.desktop_windows,
-                        size: 40,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 20),
-                  Focus(
-                    focusNode: _icon3FocusNode,
-                    child: Container(
-                      padding: const EdgeInsets.all(12),
-                      decoration: BoxDecoration(
-                        border: !_icon3FocusNode!.hasFocus
-                            ? null
-                            : Border.all(
-                                width: 5,
-                                color: const Color.fromARGB(255, 41, 141, 255),
-                              ),
-                      ),
-                      child: const Icon(
-                        Icons.desktop_mac,
-                        size: 40,
+                        child: const Icon(
+                          Icons.laptop,
+                          size: 40,
+                        ),
                       ),
                     ),
                   ),
                   const SizedBox(width: 20),
-                  Focus(
-                    focusNode: _icon4FocusNode,
-                    child: Container(
-                      padding: const EdgeInsets.all(12),
-                      decoration: BoxDecoration(
-                        border: !_icon4FocusNode!.hasFocus
-                            ? null
-                            : Border.all(
-                                width: 5,
-                                color: const Color.fromARGB(255, 41, 141, 255),
-                              ),
+                  Actions(
+                    actions: <Type, Action<Intent>>{
+                      LeftButtonIntent:
+                          CallbackAction<LeftButtonIntent>(onInvoke: (Intent) {
+                        _selectdIcon = Icons.laptop;
+                        return _changeFocus(context, _icon1FocusNode!);
+                      }),
+                      RightButtonIntent:
+                          CallbackAction<RightButtonIntent>(onInvoke: (Intent) {
+                        _selectdIcon = Icons.desktop_mac;
+                        return _changeFocus(context, _icon3FocusNode!);
+                      }),
+                         UpButtonIntent:
+                          CallbackAction<UpButtonIntent>(onInvoke: (Intent) {
+                        _selectdIcon = Icons.play_arrow;
+                        return _changeFocus(context, _playButtonFocusNode);
+                      }),
+                      DownButtonIntent:
+                          CallbackAction<DownButtonIntent>(onInvoke: (Intent) {
+                        _selectdIcon = Icons.image;
+                        return _changeFocus(context, _carouselFocusNode);
+                      }),
+                    },
+                    child: Focus(
+                      focusNode: _icon2FocusNode,
+                      child: Container(
+                        padding: const EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          border: !_icon2FocusNode!.hasFocus
+                              ? null
+                              : Border.all(
+                                  width: 5,
+                                  color:
+                                      const Color.fromARGB(255, 41, 141, 255),
+                                ),
+                        ),
+                        child: const Icon(
+                          Icons.desktop_windows,
+                          size: 40,
+                        ),
                       ),
-                      child: const Icon(
-                        Icons.tablet_android,
-                        size: 40,
+                    ),
+                  ),
+                  const SizedBox(width: 20),
+                  Actions(
+                    actions: <Type, Action<Intent>>{
+                      LeftButtonIntent:
+                          CallbackAction<LeftButtonIntent>(onInvoke: (Intent) {
+                        _selectdIcon = Icons.desktop_windows;
+                        return _changeFocus(context, _icon2FocusNode!);
+                      }),
+                      RightButtonIntent:
+                          CallbackAction<RightButtonIntent>(onInvoke: (Intent) {
+                        _selectdIcon = Icons.tablet_android;
+                        return _changeFocus(context, _icon4FocusNode!);
+                      }),
+                         UpButtonIntent:
+                          CallbackAction<UpButtonIntent>(onInvoke: (Intent) {
+                        _selectdIcon = Icons.play_arrow;
+                        return _changeFocus(context, _playButtonFocusNode);
+                      }),
+                      DownButtonIntent:
+                          CallbackAction<DownButtonIntent>(onInvoke: (Intent) {
+                        _selectdIcon = Icons.image;
+                        return _changeFocus(context, _carouselFocusNode);
+                      }),
+                    },
+                    child: Focus(
+                      focusNode: _icon3FocusNode,
+                      child: Container(
+                        padding: const EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          border: !_icon3FocusNode!.hasFocus
+                              ? null
+                              : Border.all(
+                                  width: 5,
+                                  color:
+                                      const Color.fromARGB(255, 41, 141, 255),
+                                ),
+                        ),
+                        child: const Icon(
+                          Icons.desktop_mac,
+                          size: 40,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 20),
+                  Actions(
+                    actions: <Type, Action<Intent>>{
+                      LeftButtonIntent:
+                          CallbackAction<LeftButtonIntent>(onInvoke: (Intent) {
+                        _selectdIcon = Icons.desktop_mac;
+                        return _changeFocus(context, _icon3FocusNode!);
+                      }),
+                      RightButtonIntent:
+                          CallbackAction<RightButtonIntent>(onInvoke: (Intent) {
+                        _selectdIcon = Icons.laptop;
+                        return _changeFocus(context, _icon1FocusNode!);
+                      }),
+                         UpButtonIntent:
+                          CallbackAction<UpButtonIntent>(onInvoke: (Intent) {
+                        _selectdIcon = Icons.play_arrow;
+                        return _changeFocus(context, _playButtonFocusNode);
+                      }),
+                      DownButtonIntent:
+                          CallbackAction<DownButtonIntent>(onInvoke: (Intent) {
+                        _selectdIcon = Icons.image;
+                        return _changeFocus(context, _carouselFocusNode);
+                      }),
+                    },
+                    child: Focus(
+                      focusNode: _icon4FocusNode,
+                      child: Container(
+                        padding: const EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          border: !_icon4FocusNode!.hasFocus
+                              ? null
+                              : Border.all(
+                                  width: 5,
+                                  color:
+                                      const Color.fromARGB(255, 41, 141, 255),
+                                ),
+                        ),
+                        child: const Icon(
+                          Icons.tablet_android,
+                          size: 40,
+                        ),
                       ),
                     ),
                   ),
